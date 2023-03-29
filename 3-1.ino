a3f82e3fe5a9d6f35dc039800469be37fed8807d
goeuddeum <125548154+goeuddeum@users.noreply.github.com>
2023-03-29 17:09:14 +0900

    Add files via upload

---
+++ 3-1.ino
@@ -0,0 +1,22 @@
+const int led[6]{3,5,6,9,10,11};
+const int buttonPin=2;
+void setup(){
+  for(int x=0;x<=5;x++){
+    pinMode(led[x],OUTPUT);
+  }
+  pinMode(buttonPin,INPUT);
+}
+void loop(){
+  int buttonInput=digitalRead(buttonPin);
+  if(buttonInput==HIGH){
+    for(int x=0;x<=5;x++){
+      for(int x=0;x<=5;x++){
+        digitalWrite(led[x],HIGH);
+        }
+      digitalWrite(led[x],LOW);
+      buttonInput = digitalRead(buttonPin);
+      if(buttonInput==LOW) break;
+      delay(50);
+    }
+  }
+}
